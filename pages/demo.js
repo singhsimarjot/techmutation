a10d7c91cc79d881ea39912961e510dda1d52f42
singhsimarjot <simarjots9@gmail.com>
2022-08-19 11:05:01 +0530

    Files deleted

--- pages/demo.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Demo() {
-  return (
-    <div> this is demo page</div>
-  )
-}
